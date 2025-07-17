d490f618384e22a137de110f345d5eb10b6d0eef
stilooz <stilooz@yandex.ru>
2025-07-17 11:30:31 +0300

    добавлены импорты

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,15 +1,28 @@
-import { ConstructorPage } from '@pages';
-import '../../index.css';
-import styles from './app.module.css';
-
-import { AppHeader } from '@components';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ConstructorPage } from '../../pages/constructor-page/constructor-page';
+import { Feed } from '../../pages/feed/feed';
+import { Login } from '../../pages/login/login';
+import { Register } from '../../pages/register/register';
+import { ForgotPassword } from '../../pages/forgot-password/forgot-password';
+import { ResetPassword } from '../../pages/reset-password/reset-password';
+import { Profile } from '../../pages/profile/profile';
+import { ProfileOrders } from '../../pages/profile-orders/profile-orders';
+import { NotFound404 } from '../../pages/not-fount-404/not-fount-404';
 
 const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/reset-password' element={<ResetPassword />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/profile/orders' element={<ProfileOrders />} />
+      <Route path='*' element={<NotFound404 />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
-//test
