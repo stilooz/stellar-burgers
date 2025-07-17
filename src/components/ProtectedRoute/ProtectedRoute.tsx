0ea36791ff2980001c590ecf4a0420e3fc75a568
stilooz <stilooz@yandex.ru>
2025-07-17 17:14:05 +0300

    еще редакс в оставшиеся компоненты

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,13 +1,19 @@
 import { ReactNode } from 'react';
 import { useSelector } from '../../services/store';
 import { Navigate } from 'react-router-dom';
+import { Preloader } from '../ui/preloader';
 
 interface ProtectedRouteProps {
   children: ReactNode;
 }
 
 export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const { isAuthenticated, authChecked } = useSelector((state) => state.auth);
+
+  if (!authChecked) {
+    return <Preloader />;
+  }
+
   if (!isAuthenticated) {
     return <Navigate to='/login' replace />;
   }
