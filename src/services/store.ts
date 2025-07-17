5d18390d5aa79631d8c1cbc7a130739c2982821b
stilooz <stilooz@yandex.ru>
2025-07-17 11:42:04 +0300

    Конфигурация Store

--- src/services/store.ts
+++ src/services/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import {
   TypedUseSelectorHook,
   useDispatch as dispatchHook,
-  useSelector as selectorHook
+  useSelector as selectorHook,
+  Provider
 } from 'react-redux';
 import { rootReducer } from './rootReducer';
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
   devTools: process.env.NODE_ENV !== 'production'
 });
 
@@ -15,4 +17,5 @@ export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
 export const useDispatch: () => AppDispatch = () => dispatchHook();
 export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
+export { Provider };
 export default store;
