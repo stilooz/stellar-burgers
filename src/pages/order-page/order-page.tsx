bfe419c976c33fabe2be941766e356eb1c413d70
stilooz <stilooz@yandex.ru>
2025-07-18 23:22:31 +0300

    исправил ссылки

--- src/pages/order-page/order-page.tsx
+++ src/pages/order-page/order-page.tsx
@@ -1,30 +1,9 @@
 import { FC } from 'react';
-import { useParams } from 'react-router-dom';
-import { useSelector } from '../../services/store';
-import { Preloader } from '@ui';
 import { OrderInfo } from '@components';
 import styles from './order-page.module.css';
 
-export const OrderPage: FC = () => {
-  const { number } = useParams<{ number: string }>();
-  const { orderDetails, loading } = useSelector((state) => state.orders);
-
-  if (loading) {
-    return <Preloader />;
-  }
-
-  if (!orderDetails && !loading) {
-    return (
-      <div className={styles.container}>
-        <h2 className='text text_type_main-large'>Заказ не найден</h2>
-      </div>
-    );
-  }
-
-  return (
-    <div className={styles.container}>
-      <h2 className='text text_type_main-large mb-5'>Информация о заказе</h2>
-      <OrderInfo />
-    </div>
-  );
-};
+export const OrderPage: FC = () => (
+  <div className={styles.container}>
+    <OrderInfo />
+  </div>
+);
